4f9918c6c84c47ca3e85479344f71d1d948e881d
Alexandre Farias <mail.alefarias@gmail.com>
2018-11-21 21:53:00 -0200

    contract query

--- src/router.js
+++ src/router.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Homepage from './views/Homepage.vue'
 import EHealthLogin from './views/EHealthLogin.vue'
+import TestingEthereum from './views/TestingEthereum.vue'
 
 Vue.use(Router)
 
@@ -16,6 +17,11 @@ export default new Router({
       path: '/ehealth',
       name: 'ehealth-login',
       component: EHealthLogin
+    },
+    {
+      path: '/ethereum',
+      name: 'testing-ethereum',
+      component: TestingEthereum
     }
   ]
 })
