393cb92450e3375ec96d9408104b6e5658f1c3ee
Alexandre Farias <mail.alefarias@gmail.com>
2018-11-26 12:13:19 -0200

    new build

--- src/router.js
+++ src/router.js
@@ -2,7 +2,6 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Homepage from './views/Homepage.vue'
 import EHealthLogin from './views/EHealthLogin.vue'
-import TestingEthereum from './views/TestingEthereum.vue'
 import Doctor from './views/Doctor.vue'
 import Patient from './views/Patient.vue'
 
@@ -19,13 +18,7 @@ export default new Router({
       path: '/ehealth',
       name: 'ehealth-login',
       component: EHealthLogin
-    },
-    {
-      path: '/ethereum-debug',
-      name: 'testing-ethereum',
-      component: TestingEthereum
-    },
-    {
+    },{
       path: '/medico',
       name: 'medico',
       component: Doctor
